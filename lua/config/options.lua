6291314b8161478fd699bdc1ce0b682fb392198a
Shawn-Summer <362847502@qq.com>
2025-11-05 09:54:42 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,6 +7,8 @@ vim.opt.clipboard = "unnamedplus"
 
 vim.opt.relativenumber = true -- use relativ number
 vim.opt_local.spell = false -- disable spell check
+vim.diagnostic.enable(false) 
+vim.g.autoformat = false -- disable autoformat 只使用 format_on_save
 ---lspconfig
 -- LSP Server to use for Python.
 -- Set to "basedpyright" to use basedpyright instead of pyright.
@@ -17,3 +19,11 @@ vim.g.lazyvim_python_ruff = "ruff"
 -- Enable the option to require a Prettier config file
 -- If no prettier config file is found, the formatter will not be used
 vim.g.lazyvim_prettier_needs_config = false
+
+-- 全局开关：是否开启保存时自动格式化（默认开启）
+vim.g.format_on_save = false
+vim.api.nvim_create_user_command("FormatOnSaveToggle", function()
+  vim.g.format_on_save = not vim.g.format_on_save
+  local status = vim.g.format_on_save and "enabled" or "disabled"
+  vim.notify("Format on save " .. status, vim.log.levels.INFO)
+end, { desc = "Toggle format on save" })
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,6 +2,7 @@ return {
   "stevearc/conform.nvim",
   opts = {
     -- 只添加这一行，其他配置会自动继承（如 formatters_by_ft、formatters 等）
+    -- 关掉只使用自己写的 format_on_save
     format_on_save = nil,
     format_after_save = nil,
   },
